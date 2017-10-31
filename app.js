39c0024ecfb295ba76cbc36401b8fe2df983c7f1
moeabdol <mohd.a.saed@gmail.com>
2017-10-31 14:29:53 +0300

    Encode user input

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs   = require('yargs');
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+}).help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+// 1301 lombard street
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20piladelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
