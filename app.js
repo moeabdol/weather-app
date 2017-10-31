19e3f725399439387d116aefd4ee95a926a69b96
moeabdol <mohd.a.saed@gmail.com>
2017-10-31 14:09:18 +0300

    Read lat and lng of location

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20piladelphia',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
+  // console.log(JSON.stringify(response, undefined, 2));
+  // console.log(JSON.stringify(error, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
